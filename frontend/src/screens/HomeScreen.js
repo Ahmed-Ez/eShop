9a5530751c149240095d421af8e246130c11500e
Ahmed Ezzat <moreone25@yahoo.com>
2020-10-22 03:44:52 +0200

    Refactor Frontend to fetch data from backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import products from '../products';
+import React, { useState, useEffect } from 'react';
 import Product from '../components/Product';
 import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+
+    getProducts();
+  }, []);
   return (
     <>
       <h1>Products</h1>
