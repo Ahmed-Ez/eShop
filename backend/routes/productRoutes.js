6ce87e376f9f0128269e7936ec84e403be9e4414
Ahmed Ezzat <moreone25@yahoo.com>
2020-10-23 00:51:46 +0200

    Setup error handling middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -16,7 +16,10 @@ router.get(
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
     if (product) return res.json(product);
-    return res.status(404).json({ message: 'Product not found' });
+    else {
+      res.status(404);
+      throw new Error('Product not found');
+    }
   })
 );
 
