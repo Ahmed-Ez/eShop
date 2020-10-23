35557cac239610cce86bf74a5da26f1954575959
Ahmed Ezzat <moreone25@yahoo.com>
2020-10-24 01:09:59 +0200

    add product details reducer and actions

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -15,8 +15,10 @@ router.get(
   '/:id',
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
-    if (product) return res.json(product);
-    else {
+
+    if (product) {
+      return res.json(product);
+    } else {
       res.status(404);
       throw new Error('Product not found');
     }
--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -17,3 +17,21 @@ export const listProducts = () => async (dispatch) => {
     });
   }
 };
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    console.log('action');
+    dispatch({ type: 'PRODUCT_DETAILS_REQUEST' });
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({ type: 'PRODUCT_DETAILS_SUCCESS', payload: data });
+  } catch (error) {
+    dispatch({
+      type: 'PRODUCT_DETAILS_FAIL',
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
